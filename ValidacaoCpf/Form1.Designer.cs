005ab8637b0fb65ef7bfd94f399a88cb2409266a
MiguelBizzi <106081382+MiguelBizzi@users.noreply.github.com>
2022-07-02 00:56:24 -0300

    primeiro commit

---
+++ ValidacaoCpf/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ValidacaoCpf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCpf = new System.Windows.Forms.Label();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.txtSituacao = new System.Windows.Forms.TextBox();
+            this.lblSituacao = new System.Windows.Forms.Label();
+            this.txtUf = new System.Windows.Forms.TextBox();
+            this.lblUf = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCpf
+            // 
+            this.lblCpf.AutoSize = true;
+            this.lblCpf.Location = new System.Drawing.Point(165, 64);
+            this.lblCpf.Name = "lblCpf";
+            this.lblCpf.Size = new System.Drawing.Size(30, 13);
+            this.lblCpf.TabIndex = 0;
+            this.lblCpf.Text = "CPF:";
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(12, 80);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(351, 20);
+            this.txtCpf.TabIndex = 1;
+            // 
+            // txtSituacao
+            // 
+            this.txtSituacao.Location = new System.Drawing.Point(12, 137);
+            this.txtSituacao.Name = "txtSituacao";
+            this.txtSituacao.Size = new System.Drawing.Size(351, 20);
+            this.txtSituacao.TabIndex = 3;
+            // 
+            // lblSituacao
+            // 
+            this.lblSituacao.AutoSize = true;
+            this.lblSituacao.Location = new System.Drawing.Point(160, 121);
+            this.lblSituacao.Name = "lblSituacao";
+            this.lblSituacao.Size = new System.Drawing.Size(52, 13);
+            this.lblSituacao.TabIndex = 2;
+            this.lblSituacao.Text = "Situação:";
+            // 
+            // txtUf
+            // 
+            this.txtUf.Location = new System.Drawing.Point(12, 185);
+            this.txtUf.Name = "txtUf";
+            this.txtUf.Size = new System.Drawing.Size(351, 20);
+            this.txtUf.TabIndex = 5;
+            // 
+            // lblUf
+            // 
+            this.lblUf.AutoSize = true;
+            this.lblUf.Location = new System.Drawing.Point(165, 169);
+            this.lblUf.Name = "lblUf";
+            this.lblUf.Size = new System.Drawing.Size(24, 13);
+            this.lblUf.TabIndex = 4;
+            this.lblUf.Text = "UF:";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(12, 230);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(351, 23);
+            this.btnVerificar.TabIndex = 6;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(12, 259);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(351, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 297);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.txtUf);
+            this.Controls.Add(this.lblUf);
+            this.Controls.Add(this.txtSituacao);
+            this.Controls.Add(this.lblSituacao);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.lblCpf);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCpf;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.TextBox txtSituacao;
+        private System.Windows.Forms.Label lblSituacao;
+        private System.Windows.Forms.TextBox txtUf;
+        private System.Windows.Forms.Label lblUf;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ ValidacaoCpf/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidacaoCpf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtCpf.Clear();
+            txtSituacao.Clear();
+            txtUf.Clear();
+            txtCpf.Focus();
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            string cpf = txtCpf.Text;
+            if (cpf.Length == 11 || cpf.Length == 14)
+            {
+                cpf = cpf.Replace(".", "");
+                cpf = cpf.Replace("-", "");
+
+                int[] d = new int[11];
+                int soma = 0;
+                int somasegundo = 0;
+                for (int i = 0; i < 11; i++)
+                {
+                    d[i] = int.Parse(cpf.Substring(i, 1));
+                }
+                for (int i = 10; i >= 2; i--)
+                {
+                    soma += d[10 - i] * i;
+                }
+
+                int resto;
+                resto = soma % 11;
+                int digito;
+
+                if (resto < 2)
+                {
+                    digito = 0;
+                }
+                else
+                {
+                    digito = 11 - resto;
+                }
+              
+
+                // SEGUNDO DIGITO 
+
+                
+                int resto2;
+                int soma2 = 0;
+                
+                int digito2;
+
+                if(digito == d[9])
+                {
+                    for (int i = 0; i < 11; i++)
+                    {
+                        d[i] = int.Parse(cpf.Substring(i, 1));
+                    }
+                    for (int i = 11; i >= 2; i--)
+                    {
+                        soma2 += d[11 - i] * i;
+
+                    }
+                    resto2 = soma2 % 11;
+
+                    if (resto2 < 2)
+                    {
+                        digito2 = 0;
+                    }
+                    else
+                    {
+                        digito2 = 11 - resto2;
+                    }
+                    txtSituacao.Text = soma.ToString() + " Digito1:" + digito + " Digito2:" + digito2;
+
+                    switch (d[8])
+                    {
+                        case 1:
+                            txtUf.Text = "Distrito Federal, Goiás, Mato Grosso do Sul e Tocantins;";
+                            break;
+                        case 2:
+                            txtUf.Text = "Pará, Amazonas, Acre, Amapá, Rondônia e Roraima;";
+                            break;
+                        case 3:
+                            txtUf.Text = "Ceará, Maranhão e Piauí;";
+                            break;
+                        case 4:
+                            txtUf.Text = "Pernambuco, Rio Grande do Norte, Paraíba e Alagoas;";
+                            break;
+                        case 5:
+                            txtUf.Text = "Bahia e Sergipe;";
+                            break;
+                        case 6:
+                            txtUf.Text = "Minas Gerais;";
+                            break;
+                        case 7:
+                            txtUf.Text = "Rio de Janeiro e Espírito Santo;";
+                            break;
+                        case 8:
+                            txtUf.Text = "São Paulo;";
+                            break;
+                        case 9:
+                            txtUf.Text = "Paraná e Santa Catarina;";
+                            break;
+                        case 0:
+                            txtUf.Text = "Rio Grande do Sul.";
+                            break;
+                    }
+
+                }
+                
+
+
+
+
+            }
+            else
+            {
+                txtCpf.Text = "CPF INVALIDO";
+            }
+
+          
+
+
+            }
+        }
+    }
+
